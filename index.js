4aba3b607ecdc72d97f892d771806b0ede26e2d7
FAHIMABBAS HARIYANI <fheemabbas@gmail.com>
2021-01-23 01:16:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("welcome to node js");
+});
+const CONNECTION_URL =
+  "mongodb+srv://admin:admin123456@cluster0.aher6.mongodb.net/demoDB?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log("server runnin on :", PORT)))
+  .catch((err) => console.log(err));
+
+mongoose.set("useFindAndModify", false);
