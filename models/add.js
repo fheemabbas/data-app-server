3d003f4514ccaaa0de128e4d9a4c4c5e7bb913b7
FAHIMABBAS HARIYANI <fheemabbas@gmail.com>
2021-01-23 09:51:46 +0530

    add without schema added

--- models/add.js
+++ models/add.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+// With schema porperty define
 const addDataSchema = mongoose.Schema({
   name: String,
   address: String,
@@ -8,6 +8,18 @@ const addDataSchema = mongoose.Schema({
     default: new Date(),
   },
 });
+//without schema define
+const addDataNoSchema = mongoose.Schema(
+  { any: {} },
+  {
+    strict: false,
+    CreateAt: {
+      type: Date,
+      default: new Date(),
+    },
+    versionKey: false,
+  }
+);
 
 const DataMessage = mongoose.model("DataMessage", addDataSchema);
 
